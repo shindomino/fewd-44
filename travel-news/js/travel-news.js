b18206ef76852d50fb3f91b504bd9fe338bde5ce
shindomino <kirby.tate91@gmail.com>
2016-05-12 19:07:48 -0700

    updated 5/12

---
+++ travel-news/js/travel-news.js
@@ -0,0 +1,7 @@
+var parent = document.querySelector(".sponsored-articles");
+
+var child = document.createElement("li");
+
+parent.appendChild(child);
+
+child.textContent = "Trump wins 37 states! NEW USA PREZ!!";
